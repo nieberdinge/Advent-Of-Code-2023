e0fa67ef3d58f878b93e4d351d918493ab3cada6
nieberdinge <33550667+nieberdinge@users.noreply.github.com>
2023-12-03 20:49:34 -0500

    path

--- day1/day1.py
+++ day1/day1.py
@@ -3,7 +3,7 @@ import re
 numbers = ["one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9"]
 conversion = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
-f = open('day1.txt')
+f = open('day1/day1.txt')
 
 total = 0
 for line in f.readlines():
