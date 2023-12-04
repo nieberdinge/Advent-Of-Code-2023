b1a42a04974e9d01a2cdc56c03ed94fd6b249f3f
nieberdinge <33550667+nieberdinge@users.noreply.github.com>
2023-12-03 21:52:22 -0500

    runs

--- day3/day3.py
+++ day3/day3.py
@@ -6,8 +6,13 @@ total = 0
 engine = f.readlines()
 
 def isValid(line, start, stop):
-    if line < 0 or line >= len(engine[line]):
+    if line < 0 or line >= len(engine):
         return False
+    if start < 0:
+        start+=1
+    if stop >= len(engine[line]):
+        stop-=1
+
     sub = engine[line][start:stop]
     sub = re.sub(r"\.","", sub)
     sub = re.sub(r"\d","", sub)
@@ -16,9 +21,10 @@ def isValid(line, start, stop):
     return False
 
 def isSpecial(index, line):
-    if index < 0 or index > len(line):
+    if index < 0 or index >= len(line):
         return False
-    sub = re.sub(r"\.","", sub)
+    letter = line[index]
+    sub = re.sub(r"\.","", letter)
     sub = re.sub(r"\d","", sub)
     if len(sub) > 0:
         return True
@@ -28,12 +34,12 @@ def isSpecial(index, line):
 for x in range(len(engine)):
     line = engine[x].strip()
     for number in re.finditer("\d+", line):
-        before = isValid(x-1, number.start(), number.end())
-        after = isValid(x+1, number.start(), number.end())
+        before = isValid(x-1, number.start()-1, number.end()+1)
+        after = isValid(x+1, number.start()-1, number.end()+1)
         left = isSpecial(number.start()-1, line)
         right = isSpecial(number.end()+1, line)
         if before or after or left or right:
-            total += int(number)
+            total += int(engine[x][number.start():number.end()])
 
 
 
