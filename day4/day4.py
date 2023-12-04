64f32164d6f17a47b48c97994a2c07901130b432
nieberdinge <33550667+nieberdinge@users.noreply.github.com>
2023-12-04 17:34:44 -0500

    working pt 1

--- day4/day4.py
+++ day4/day4.py
@@ -4,12 +4,10 @@ total = 0
 
 for line in f.readlines():
     numbers = line.strip().split(':')[1]
-    lottery = numbers.strip().split('|')[0].strip().split(' ')
-    yours = numbers.strip().split('|')[1].strip().split(' ')
+    lottery = list(filter(None, numbers.strip().split('|')[0].strip().split(' ')))
+    yours = list(filter(None, numbers.strip().split('|')[1].strip().split(' ')))
     winningNums = [num for num in yours if num in lottery]
-    total += 2 ** len(winningNums)-1
+    total += 2 ** (len(winningNums)-1) if len(winningNums) > 0 else 0
 
 print("The total is:", total)
     
-
-
