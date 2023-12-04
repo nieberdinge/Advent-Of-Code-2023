82f509052eb712fc81eb2b4cce23d4b4bb9aa804
nieberdinge <33550667+nieberdinge@users.noreply.github.com>
2023-12-03 21:45:31 -0500

    first solution

---
+++ day3/day3.py
@@ -0,0 +1,41 @@
+import re 
+
+f = open('day3/day3.txt')
+total = 0
+
+engine = f.readlines()
+
+def isValid(line, start, stop):
+    if line < 0 or line >= len(engine[line]):
+        return False
+    sub = engine[line][start:stop]
+    sub = re.sub(r"\.","", sub)
+    sub = re.sub(r"\d","", sub)
+    if len(sub) > 0:
+        return True
+    return False
+
+def isSpecial(index, line):
+    if index < 0 or index > len(line):
+        return False
+    sub = re.sub(r"\.","", sub)
+    sub = re.sub(r"\d","", sub)
+    if len(sub) > 0:
+        return True
+    return False
+
+
+for x in range(len(engine)):
+    line = engine[x].strip()
+    for number in re.finditer("\d+", line):
+        before = isValid(x-1, number.start(), number.end())
+        after = isValid(x+1, number.start(), number.end())
+        left = isSpecial(number.start()-1, line)
+        right = isSpecial(number.end()+1, line)
+        if before or after or left or right:
+            total += int(number)
+
+
+
+print("the total is", total)
+f.close()
